19c968be48256b92365a6b937b4d5de45e504916
David Dang <ddang.sw@gmail.com>
2016-03-15 22:39:07 -0700

    More coding to get additional feature working: Use MDL card in place of modal (for now) until we can get a modal solution or change to bootstrap Use template and use ajax to load them as needed Modify code to define some draft struture of all the pieces that we need access via variable Modify code to add rough functionality of signin    On successful login, it will add a id and email of the users on the right side of the footer (for now)    Also attempt to hide some of the buttons such as signup Modify code to add a rough functionality of logout    On logout, clear the authenticated info in the footer (right side)    Also toggle the button back to Login and show the other buttons
    Please start using db.json and not photos_json.js as it contains users/photos and other mockup.

--- js/photothief.js
+++ js/photothief.js
@@ -1,78 +1,192 @@
 // Client-side code
 /* jshint browser: true, jquery: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, latedef: true, newcap: true, nonew: true, quotmark: double, undef: true, unused: true, strict: true, trailing: true */
-/* global alert: true */
+/* global alert: true, console: true, componentHandler: true */
 
 var main = function () {
     "use strict";
 
     /*
-        Declare a set of variable for the Actions to make it easy to change
-        Legend:
-        variable with $ indicate a jQuery object
-        variable without $ indicate normal variable
+        Change the individual variable into a object with tree structure like
+        this allow us to group all the fields, actions and allow easy access
+        to the individual item accordingly.
+
+        i.e. to select the login button: $($pt.landPage.login)
+
     */
-    var navigationAction = ".pt_navAction";
-    var signInOutAction = ".pt_signInOutAction";
-    var signUpAction = ".pt_signUpAction";
-    var demandsAction = ".pt_demandAction";
+    var $pt = {
+        landPage: {
+            navbar : ".pt_navAction",
+            content: ".pt_landPage-content",
+            login : ".pt_openLoginCardAction",
+            logout : ".pt_logOutAction",
+            signup : ".pt_openSignupCardAction",
+            demand : ".pt_demandAction",
+            loginInfo: ".pt_landPage-loginInfo",
+            authenId: "#pt_authenticated_Id",
+            authenEmail: "#pt_authenticated_Email"
+        },
+
+        loginCard: {
+            handle: ".pt_loginCard",
+            userId : "#pt_loginCard-loginId",
+            userPass: "#pt_loginCard-loginPassword",
+            revealPass: "#pt_loginCard-revealPassword",
+            login: ".pt_loginAction",
+            errorStatus: "#pt_loginCard-errorMessage"
+        }
+    };
+
+    // Function to load the Picture Carousel with specified AJAX source
+    // and the sliderId
+    function loadCarousel($sliderId, source) {
+        // Load owlCarousel for slider 1.  Sample data pull from image 40 to 60
+        $($sliderId).owlCarousel({
+            jsonPath : source,
+            jsonSuccess : function (data) {
+                // Call back function to process the photo data we pull
+                data.forEach(function(image) {
+                    var $img = $("<img>");
+                    $img.attr("src", image.src);
+                    $img.attr("alt", "images from json");
+                    $($sliderId).append($img);
+                });
+            },
+            navigation: true,
+            navigationText: [
+              "<i class='material-icons'>navigate_before</i>",
+              "<i class='material-icons'>navigate_next</i>" ],
+            autoPlay : 5000, //Set AutoPlay to 3 seconds
+            items : 8,
+            lazyLoad : true,
+            itemsDesktop : [1199,5],
+            itemsDesktopSmall : [979,3]
+        });
+    }
 
     // Function for the initial load of the page.
     function initialize() {
 
-        // Load owlCarousel for slider 1.  Sample data pull from image 40 to 60
-        $("#pt_photoSlider1").owlCarousel({
-            jsonPath : "http://localhost:3000/photos?_start=40&_end=60",
-            jsonSuccess : function (data) {
-                // Call back function to process the photo data we pull
-                data.forEach(function(image) {
-                    var $img = $("<img>");
-                    $img.attr("src", image.src);
-                    $img.attr("alt", "images from json");
-                    $("#pt_photoSlider1").append($img);
-                });
+        // TODO: More code to handle the sign up
+        var $template = $("<div class=\"mdl-grid\">").hide();
+
+        // We define the carousel object
+        var carousel = {
+            mostWanted: {
+                slider: "#pt_photoSlider1",
+                source: "http://localhost:3000/photos?_start=40&_end=60"
             },
-            navigation: true,
-            navigationText: [
-              "<i class='material-icons'>navigate_before</i>",
-              "<i class='material-icons'>navigate_next</i>" ],
-            autoPlay : 5000, //Set AutoPlay to 3 seconds
-            items : 8,
-            lazyLoad : true,
-            itemsDesktop : [1199,5],
-            itemsDesktopSmall : [979,3]
+            newestUpload: {
+                slider: "#pt_photoSlider2",
+                source: "http://localhost:3000/photos?_start=0&_end=30"
+            }
+        };
+
+        // Use AJAX to load in the template file with the main carousel
+        $template.load("templates/main.tmpl", function (result, status) {
+            if (status === "success") {
+                // Empty all stuff inside the land page content
+                $($pt.landPage.content).empty();
+                // Add the newly add template login into the land page content
+                $($pt.landPage.content).append($template);
+
+                // MDL update it
+                componentHandler.upgradeAllRegistered();
+
+                // Loop to load carousel
+                for (var key in carousel) {
+                    if (carousel.hasOwnProperty(key)) {
+                        var slider = carousel[key].slider;
+                        var source = carousel[key].source;
+                        loadCarousel(slider, source);
+                    }
+                }
+
+                // Show the carousel
+                $template.fadeIn(300);
+
+            } else {
+                // Encountered error
+                console.log(status, "Unable to load templates/main.tmpl");
+            }
         });
-
-        // Load owlCarousel for slider 2.  Sample data pull from image 0 to 25
-        $("#pt_photoSlider2").owlCarousel({
-            jsonPath : "http://localhost:3000/photos?_start=0&_end=30",
-            jsonSuccess : function (data) {
-                // Call back function to process the photo data we pull
-                data.forEach(function(image) {
-                    var $img = $("<img>");
-                    $img.attr("src", image.src);
-                    $img.attr("alt", "images from json");
-                    $("#pt_photoSlider2").append($img);
-                });
-            },
-            navigation: true,
-            navigationText: [
-              "<i class='material-icons'>navigate_before</i>",
-              "<i class='material-icons'>navigate_next</i>" ],
-            autoPlay : 5000, //Set AutoPlay to 3 seconds
-            items : 8,
-            lazyLoad : true,
-            itemsDesktop : [1199,5],
-            itemsDesktopSmall : [979,3]
-        });
-
-
 
         // TODO: Any other tasks need to be done here to initialize page
     } // End iniitalize function
 
+    function processSuccessLogin(result) {
+        // Success Login save the id and the loginId to the footer
+        // Can't save to session or anything here
+        $($pt.landPage.authenId).text(result[0].id);
+        $($pt.landPage.authenEmail).text(result[0].email);
+
+        // We hide the loginCard
+        $($pt.loginCard.handle).fadeOut(300).remove();
+        // Hide signup button
+        $($pt.landPage.signup).fadeOut();
+        // Toggle the login Button to say logout
+        $($pt.landPage.login).addClass(($pt.landPage.logout).substr(1));
+        $($pt.landPage.login).text("Logout");
+        $($pt.landPage.login).removeClass(($pt.landPage.login).substr(1));
+
+        // Reload the main page with carousel
+        initialize();
+    }
+    // Function to handle the loginCard login button
+    function handleLoginAction() {
+        //var $target = $(event.currentTarget);
+        // Grab all the fields on the login Card
+        var $userId = $($pt.loginCard.userId);
+        var $userPass = $($pt.loginCard.userPass);
+        var $revealPass = $($pt.loginCard.revealPass);
+        var $errorStatus = $($pt.loginCard.errorStatus);
+
+        if ($userId.val().trim() === "" || $userPass.val().trim() === "") {
+            // Empty login Information
+            $errorStatus.text("Either User or Password is empty");
+            return false;
+        }
+
+        // Build the JSON urlHost
+        var urlHost = "http://localhost:3000/users";
+        urlHost += "?loginId=" + $userId.val().trim();
+        urlHost += "&password=" + $userPass.val().trim();
+
+        // Clear input boxes
+        $errorStatus.text("");
+        $userId.val("");
+        $userId.parent().removeClass("is-dirty");
+        $userPass.val("");
+        $userPass.parent().removeClass("is-dirty");
+        // Reset the show password checkbox and also the password type
+        if ($revealPass.is(":checked") === true) {
+            $revealPass.prop("checked", false);
+            $revealPass.parent().removeClass("is-checked");
+            $userPass.prop("type", "password");
+        }
+
+        // TODO:  Perform AJAX to check data here
+        $.ajax({
+            url: urlHost,
+            type: "json",
+            method: "GET",
+            success: function (result) {
+                // result is an array if it return
+                if (result.length === 0) {
+                    // Failed login
+                    $($pt.loginCard.errorStatus).text("Invalid Login Information");
+                    return false;
+                } else if (result.length === 1) {
+                    processSuccessLogin(result);
+                }
+            },
+            error: function (result) {
+                console.log("ajax error " + result.status);
+            }
+        });
+    }
 
     // Event handler for Sign Up link
-    $(navigationAction).on("click", signUpAction, function (event) {
+    $($pt.landPage.signup).on("click", function (event) {
         var $target = $(event.currentTarget);
         alert ("Action click:" + $target.text());
 
@@ -81,60 +195,82 @@ var main = function () {
         // TODO: When successful, auto sign in the person
 
         // TODO: Maybe trigger auto sign in
-        $(signInOutAction).trigger("click");
+        // $(signInOutAction).trigger("click");
         // Needed to stop follow link
         return false;
     });
 
-    // Event handler for SignIn and SignOut Link
-    $(navigationAction).on("click", signInOutAction, function (event) {
+    // Even handler for Login Link
+    $($pt.landPage.navbar).on("click", $pt.landPage.login, function () {
+        //var $target = $(event.currentTarget);
+
+        // TODO: More code to handle the sign up
+        var $template = $("<div class=\"mdl-grid\">").hide();
+
+        // Use AJAX to load in the template file with the login form
+        $template.load("templates/login.tmpl", function (result, status) {
+            if (status === "success") {
+                // Empty all stuff inside the land page content
+                $($pt.landPage.content).empty();
+                // Add login Card into the land page content
+                $($pt.landPage.content).append($template);
+
+                // IMPORTANT:  Must run the command below to register the
+                // dynamic content to MDL
+                componentHandler.upgradeAllRegistered();
+
+                // Add Event handler for the login button
+                $($pt.loginCard.login).on("click", handleLoginAction);
+
+                // Add Event handler for the Checkbox toggle
+                $($pt.loginCard.revealPass).on("click", function (event) {
+                    var $target = $(event.currentTarget);
+                    // Set the type of the password input to either text or password
+                    if ($target.is(":checked") === true) {
+                        $($pt.loginCard.userPass).prop("type", "text");
+                    } else {
+                        $($pt.loginCard.userPass).prop("type", "password");
+                    }
+                });
+
+                // Show the login Card
+                $template.fadeIn(500);
+            } else {
+                // Encountered error
+                console.log(status, "Unable to load templates/login.tmpl");
+            }
+        });
+
+        // TODO: When successful, auto sign in the person
+
+        // TODO: Maybe trigger auto sign in
+        // $(signInOutAction).trigger("click");
+        // Needed to stop follow link
+        return false;
+    });
+
+    // Event handler for logout Link
+    $($pt.landPage.navbar).on("click", $pt.landPage.logout, function (event) {
         var $target = $(event.currentTarget);
-        // Toggle the value from sign in to sign out on click
-        var toggleValue;
-        var successFlag = true;
 
-        if ($target.text() === "Sign In") {
-            toggleValue = "Sign Out";
-            // TODO: Code to handle Sign In action
-            alert ("Action click:" + $target.text());
+        // Remove userId and Email from footer
+        // Can't save to session or anything here
+        $($pt.landPage.authenId).text("");
+        $($pt.landPage.authenEmail).text("");
 
-            // TODO: More code here
-
-            // Set successFlag
-            successFlag = true;
-
-            // Hide Sign Up Action
-            $(signUpAction).hide();
-
-        } else {
-            toggleValue = "Sign In";
-
-            // TODO: Code to handle Sign Out action
-            alert ("Action click:" + $target.text());
-
-            // TODO: More code here
-
-            // set successFlag
-            successFlag = true;
-
-            // Show Sign Up Action
-            $(signUpAction).fadeIn();
-        }
-
-        // Toggle Text only when the action is successfully handled
-        if (successFlag) {
-            $target.fadeOut("fast", function () {
-                $(this).text(toggleValue);
-                $(this).fadeIn();
-            });
-        }
+        // show SignUp button
+        $($pt.landPage.signup).fadeIn();
+        // Toggle the login Button to say logout
+        $($pt.landPage.logout).addClass(($pt.landPage.login).substr(1));
+        $($pt.landPage.logout).text("Login");
+        $($pt.landPage.logout).removeClass(($pt.landPage.logout).substr(1));
 
         // Needed to stop follow link
         return false;
     });
 
     // Event handler event for demands Action
-    $(navigationAction).on("click", demandsAction, function (event) {
+    $($pt.landPage.demand).on("click", function (event) {
         var $target = $(event.currentTarget);
 
         // TODO: code needed here to handle demands
