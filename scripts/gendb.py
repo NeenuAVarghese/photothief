ac4f70c2c293c94d84bdd1225afefc1a6abaeb87
mittman <mittman@users.noreply.github.com>
2016-03-23 02:27:55 -0700

    Update DB generate script

--- scripts/gendb.py
+++ scripts/gendb.py
@@ -11,8 +11,8 @@ pid = 0
 did = 0
 thieves = [ "Kevin", "David", "Neenu", "Anusha", "Chris" ]
 webmail = [ "aol", "gmail", "hotmail", "yahoo" ]
-victims = [ "Bob", "Diana", "Elektra", "Jean", "Natalia", "Selina", "Susan" ]
-words = [ "bubble", "cute", "chair", "diamond", "funny", "star", "sponge", "tall" ]
+victims = [ "Anton", "Apolonia", "Arturo", "Azalee", "Basilia", "Bertram", "Bobbie", "Bobby", "Calista", "Charlena", "Damian", "Del", "Dolly", "Doug", "Drusilla", "Eryn", "Golda", "Hertha", "Howard", "Inger", "Irwin", "Ivette", "Jeni", "Jess", "Jonell", "Kasha", "Kimberlee", "Kirsten", "Kitty", "Lin", "Luciana", "Ludie", "Mason", "Maxine", "Merlyn", "Mica", "Michiko", "Odilia", "Piper", "Shelton", "Susan", "Tanja", "Temeka", "Terisa", "Timmy", "Val", "Wava", "Wilson", "Winfred", "Yahaira" ]
+words = ["Adorable", "Amusing", "Bashful", "Brave", "Carefree", "Challenging", "Clever", "Cozy" "Crawling", "Creeping", "Cruel", "Difficult", "Easy", "Fair", "Foolish", "Friendly", "Generous", "Gloomy", "Grand", "Grimy", "Hardworking", "Hideous", "Huge", "Kind", "Loud", "Meek", "Moody", "Nasty", "Petite", "Polite", "Quick", "Rich", "Scared", "Shrill", "Sizzling", "Sloppy", "Smooth", "Sparkling", "Splendid", "Steaming", "Stingy", "Stuffed", "Swift", "Tiny", "Truthful", "Wild", "Worried"]
 ransoms = []
 
 # whitespace
@@ -58,32 +58,35 @@ def gen_users():
 	testers = len(thieves)
 	for i in range(0, testers-1):
 		newuser()
+	global n
 	n = ""
 	newuser()
+	n = ","
 
 def newphoto(oldname="000.jpg"):
 	# string format
 	photo = w + "{{" + f + "'id': {0}," + f + "'src': '{1}'," + f
 	photo += "'createDate': {2}," + f + "'score': {3}," + f
-	photo += "'source': 'own'," + f + "'used': false," + f
-	photo += "'userId': {4}" + d + "}}" + n
+	photo += "'source': 'own'," + f + "'used': {4}," + f
+	photo += "'userId': {5}" + d + "}}" + n
 
 	# generate data
 	global pid
-	rand6 = "%06d" %random.randrange(10**6)
-	timestamp = "1456" + rand6 + str(pid)
+	rand9 = "%09d" %random.randrange(10**9)
+	timestamp = "1456" + rand9
 	newfile = "photos/ptimg-" + timestamp + ".jpg"
 	score = random.randrange(30)
+	used = random.choice(["true", "false"])
 	userid = random.randrange(5)
-
-	# generate demand
-	newdemand(pid, userid)
-
-	x_print(photo.format(pid, newfile, timestamp, score, userid).replace("'", "\""))
-	pid += 1
+	x_print(photo.format(pid, newfile, timestamp, score, used, userid).replace("'", "\""))
 
 	# rename images
-	#call(["mv", "-v", "photos/" + oldname, newfile])
+	call(["mv", "-v", "photos/" + oldname, newfile])
+
+	# generate demand
+	if used == "false":
+		newdemand(pid, userid)
+	pid += 1
 
 def gen_photos():
 	gallery = []
@@ -97,8 +100,10 @@ def gen_photos():
 	total = len(gallery)
 	for i in range(0, total-1):
 		newphoto(gallery[i])
+	global n
 	n = ""
 	newphoto(gallery[total-1])
+	n = ","
 
 def newdemand(pid, uid):
 	# string format
@@ -111,18 +116,21 @@ def newdemand(pid, uid):
 	global did, ransoms
 	rand9 = "%09d" %random.randrange(10**9)
 	timestamp = "1458" + rand9
-	points = random.randrange(9999)
-	vmail = random.choice(words) + random.choice(victims).lower()
+	points = random.randrange(500)
+	vmail = random.choice(words).lower() + random.choice(victims).lower()
 	vmail += "@" + random.choice(webmail) + ".com"
 	b64sum = base64.b64encode(bytes(vmail + str(did), "utf-8"))
-	url = str(b64sum).split("'")[1].replace("=", ".")[:11]
+	url = str(b64sum).split("'")[1].replace("=", ".")#[:11]
 	ransoms.append(demand.format(did, vmail, timestamp, points, pid, uid, url).replace("'", "\""))
 	did += 1
 
 def gen_demands():
 	global ransoms
-	random.shuffle(ransoms)
-	x_print(''.join(ransoms))
+	print(ransoms[-1])
+	ransoms[-1] = ransoms[-1].rstrip(",")
+	print(ransoms[-1])
+	#random.shuffle(ransoms)
+	x_print('\n'.join(ransoms))
 
 def print_json():
 	x_print("{")
@@ -140,7 +148,7 @@ def print_json():
 	# demands
 	x_print(s + "'demands': [".replace("'", "\""))
 	gen_demands()
-	x_print(s + "],")
+	x_print(s + "]")
 
 	x_print("}")
 
