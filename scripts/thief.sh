bc8a03053efd641a6d4da9a87a34d15ab2caf438
mittman <mittman@users.noreply.github.com>
2016-04-05 07:10:35 -0700

    Update helper scripts

--- scripts/thief.sh
+++ scripts/thief.sh
@@ -1,22 +1,29 @@
-#!/bin/sh
+#!/bin/bash
+
 gitrepo="/opt/git/photothief"
 
 http=$(ps aux 2>/dev/null | grep p8000 | grep -i screen | awk '{print $2}')
 node=$(ps aux 2>/dev/null | grep n8000 | grep -i screen | awk '{print $2}')
 json=$(ps aux 2>/dev/null | grep j3000 | grep -i screen | awk '{print $2}')
+checkrepo=$(git config --get "remote.origin.url" 2>/dev/null | grep -o photothief)
 
 [ -z "$json" ] || kill $json
 [ -z "$http" ] || kill $http
 [ -z "$node" ] || kill $node
 
 if [ "$1" = "-k" -o "$1" = "--kill" ]; then
+  echo "[photothief is terminating]"
   exit 0
 elif [ ! -z "$1" ]; then
   echo -e "USAGE:  $0"
   echo -e "\t$0 --kill"
 fi
 
-cd "$gitrepo" || echo "Invalid directory $gitrepo"
+if [ -z "$checkrepo" ]; then
+  cd "$gitrepo" || echo "ERROR: Invalid directory $gitrepo"
+  [ "$checkrepo" = "photothief" ] || echo "Wrong git repo, try again"
+fi
+
 #screen -dmSL p8000 python -m http.server
 screen -dmS n8000 node server/ptserver.js
 screen -dmS j3000 json-server jsondb/db.json
--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-################
-### web-test ###
-################
 
 html="index.html"
+html2="victim/index.html"
 css="stylesheets/photothief.css"
+css2="stylesheets/victim.css"
 js="js/photothief.js"
+js2="js/victim.js"
 
 checkdep() { type -p $1 &>/dev/null; }
 
@@ -18,7 +18,9 @@ reset="\e[0m"
 if checkdep tidy; then
   echo -e "${alert}==>${reset} ${notice}Running 'tidy' HTML validation${reset}"
   output=$(tidy -qe "$html" 2>&1)
-  [ ! -z "$output" ] && echo -e "${fail}==> FAIL${reset}" && echo "$output"
+  [ ! -z "$output" ] && echo -e "${fail}==> FAIL${reset} $html" && echo "$output"
+  output2=$(tidy -qe "$html2" 2>&1)
+  [ ! -z "$output2" ] && echo -e "${fail}==> FAIL${reset} $html2" && echo "$output2"
 else
   echo -e "${warn}Missing tidy${reset}. Skipping HTML validation"
 fi
@@ -26,7 +28,9 @@ fi
 if checkdep csslint; then
   echo -e "${alert}==>${reset} ${notice}Running 'csslint' CSS validation${reset}"
   output=$(csslint --quiet "$css" 2>&1)
-  [ ! -z "$output" ] && echo -e "${fail}==> FAIL${reset}" && echo "$output"
+  [ ! -z "$output" ] && echo -e "${fail}==> FAIL${reset} $css" && echo "$output"
+  output2=$(csslint --quiet "$css2" 2>&1)
+  [ ! -z "$output2" ] && echo -e "${fail}==> FAIL${reset} $css2" && echo "$output2"
 else
   echo -e "${warn}Missing csslint${reset}. Skipping CSS validation"
 fi
@@ -34,7 +38,9 @@ fi
 if checkdep jshint; then
   echo -e "${alert}==>${reset} ${notice}Running 'jshint' Javascript validation${reset}"
   output=$(jshint "$js" 2>&1)
-  [ ! -z "$output" ] && echo -e "${fail}==> FAIL${reset}" && echo "$output"
+  [ ! -z "$output" ] && echo -e "${fail}==> FAIL${reset} $js" && echo "$output"
+  output2=$(jshint "$js2" 2>&1)
+  [ ! -z "$output2" ] && echo -e "${fail}==> FAIL${reset} $js2" && echo "$output2"
 else
   echo -e "${warn}Missing jshint${reset}. Skipping Javascript validation"
 fi
